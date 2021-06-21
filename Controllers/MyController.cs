678ba3db065c6ba09d466af602f44a085d550a1f
ls-xuwei <3517372489@qq.com>
2021-06-21 14:21:58 +0800

    首次提交

---
+++ Controllers/MyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class MyController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+		private readonly ILogger<MyController> _logger;
+
+		public MyController(ILogger<MyController> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		[HttpGet]
+		public string Get()
+		{
+			return _configuration["My:TestKey"];
+		}
+	}
+}
